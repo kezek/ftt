bc76bac29cb2b421eb6f584e4176eca58dc5eeb8
Andrei <kezek.ma@gmail.com>
2012-10-18 18:49:34 +0300

    improved design of settings dialog

--- lib/ftt/gui/settings.rb
+++ lib/ftt/gui/settings.rb
@@ -3,20 +3,25 @@ include Fox
 
 class Settings < FXDialogBox
 
-  attr_reader :GUsername,:GPassword,:hFrame3
+  attr_reader :GUsername, :GPassword, :GSpreadsheetKey
 
   def initialize(owner)
     # Invoke base class initialize function first
     super(owner, "Settings", DECOR_TITLE|DECOR_BORDER)
-    hFrame1 = FXHorizontalFrame.new(self)
-      hFrame1Label1 = FXLabel.new(hFrame1,"Google account username:")
-      @GUsername = FXTextField.new(hFrame1,30, :opts => LAYOUT_FILL)
-    hFrame2 = FXHorizontalFrame.new(self)
-      hFrame2Label = FXLabel.new(hFrame2,"Google account password:")
-      @GPassword = FXTextField.new(hFrame2,30, :opts => LAYOUT_FILL|TEXTFIELD_PASSWD)
-    FXHorizontalSeparator.new(self, SEPARATOR_GROOVE)
-    @hFrame3 = FXHorizontalFrame.new(self)
-      FXButton.new(hFrame3, "Accept", nil, self, ID_ACCEPT,
-      FRAME_RAISED|FRAME_THICK|LAYOUT_RIGHT|LAYOUT_CENTER_Y)
+      hFrame1 = FXHorizontalFrame.new(self)
+      vFrame1 = FXVerticalFrame.new(hFrame1)
+        hFrame1Label1 = FXLabel.new(vFrame1, "Google username:",:padTop => 4)
+        hFrame2Label = FXLabel.new(vFrame1, "Google password:", :padTop => 4)
+        hFrame3Label = FXLabel.new(vFrame1, "Spreadsheet Key:", :padTop => 4)
+      vFrame2 = FXVerticalFrame.new(hFrame1)
+        @GUsername = FXTextField.new(vFrame2, 45,
+          :opts => LAYOUT_CENTER_Y|LAYOUT_CENTER_X|FRAME_SUNKEN|FRAME_THICK|LAYOUT_FILL_ROW)
+        @GPassword = FXTextField.new(vFrame2, 45,
+          :opts => TEXTFIELD_PASSWD|LAYOUT_CENTER_Y|LAYOUT_CENTER_X|FRAME_SUNKEN|FRAME_THICK|LAYOUT_FILL_ROW)
+        @GSpreadsheetKey = FXTextField.new(vFrame2, 45,
+          :opts => LAYOUT_CENTER_Y|LAYOUT_CENTER_X|FRAME_SUNKEN|FRAME_THICK|LAYOUT_FILL_ROW)
+      hFrame2 = FXHorizontalFrame.new(self)
+        FXButton.new(hFrame2, "Accept", nil, self, ID_ACCEPT,
+          FRAME_RAISED|FRAME_THICK|LAYOUT_CENTER_Y)
   end
 end
