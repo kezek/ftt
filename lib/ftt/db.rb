de2be30df7bebd95ce345d8b25ef15d2ffe54fec
Andrei <kezek.ma@gmail.com>
2012-10-18 11:41:43 +0300

    bugfix : fixed relative path for db

--- lib/ftt/db.rb
+++ lib/ftt/db.rb
@@ -2,7 +2,8 @@ require 'sqlite3'
 # Module for conneting with the DB
 #singleton pattern
 module Db
-  DB_PATH = File.expand_path(File.dirname(__FILE__))
+  DB_PATH = File.expand_path("../../../data/ftt.db", __FILE__)
+
 
 
   public
@@ -11,14 +12,12 @@ module Db
   end
 
   def self.instance
-    return
     @@db
   end
 
   protected
   def self.connect
     puts 'CONNECTED'
-    puts DB_PATH
     @@db = SQLite3::Database.new(DB_PATH)
   end
 end
