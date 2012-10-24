72f43b7748f478cf2c95559ba80c9f997f2c5946
Andrei <kezek.ma@gmail.com>
2012-10-24 19:08:49 +0300

    Pre-rendering db values in the Settings dialog

--- lib/ftt/gui/settings.rb
+++ lib/ftt/gui/settings.rb
@@ -14,12 +14,33 @@ class Settings < FXDialogBox
         hFrame2Label = FXLabel.new(vFrame1, "Google password:", :padTop => 4)
         hFrame3Label = FXLabel.new(vFrame1, "Spreadsheet Key:", :padTop => 4)
       vFrame2 = FXVerticalFrame.new(hFrame1)
+        # render the field
         @GUsername = FXTextField.new(vFrame2, 45,
           :opts => LAYOUT_CENTER_Y|LAYOUT_CENTER_X|FRAME_SUNKEN|FRAME_THICK|LAYOUT_FILL_ROW)
+        # fetch config value
+        gUsernameConfigValue = Ftt::Config.instance.getGUsername
+        # display config value unless empty or nil
+        unless gUsernameConfigValue.nil? or gUsernameConfigValue == ' ' or gUsernameConfigValue.empty?
+          @GUsername.text = gUsernameConfigValue
+        end
+
+        # render the field
         @GPassword = FXTextField.new(vFrame2, 45,
           :opts => TEXTFIELD_PASSWD|LAYOUT_CENTER_Y|LAYOUT_CENTER_X|FRAME_SUNKEN|FRAME_THICK|LAYOUT_FILL_ROW)
+        # fetch config value
+        gPasswordConfigValue = Ftt::Config.instance.getGPassword
+        # display config value unless empty or nil
+        unless gPasswordConfigValue.nil? or gPasswordConfigValue == ' ' or gPasswordConfigValue.empty?
+          @GPassword.text = gPasswordConfigValue
+        end
+
+        #render the field
         @GSpreadsheetKey = FXTextField.new(vFrame2, 45,
           :opts => LAYOUT_CENTER_Y|LAYOUT_CENTER_X|FRAME_SUNKEN|FRAME_THICK|LAYOUT_FILL_ROW)
+        gSpreadsheetKeyConfigValue = Ftt::Config.instance.getGSpreadsheetKey
+        unless gSpreadsheetKeyConfigValue.nil? or gSpreadsheetKeyConfigValue == ' ' or gSpreadsheetKeyConfigValue.empty?
+          @GSpreadsheetKey.text = gSpreadsheetKeyConfigValue
+        end
       hFrame2 = FXHorizontalFrame.new(self)
         acceptButton = FXButton.new(hFrame2, "Accept", nil, self, ID_ACCEPT,
           FRAME_RAISED|FRAME_THICK|LAYOUT_CENTER_Y)
