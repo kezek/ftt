2b55cc4a459bdb65a794a3230f221a0722f50579
Andrei <kezek.ma@gmail.com>
2012-10-27 02:36:18 +0300

    Adding maconomy text box

--- lib/ftt/gui/settings.rb
+++ lib/ftt/gui/settings.rb
@@ -3,11 +3,11 @@ include Fox
 
 class Settings < FXDialogBox
 
-  attr_reader :GUsername, :GPassword, :GSpreadsheetKey
+  attr_reader :GUsername, :GPassword, :GSpreadsheetKey, :maconomyData
 
   def initialize(owner)
     # Invoke base class initialize function first
-    super(owner, "Settings", DECOR_TITLE|DECOR_BORDER)
+    super(owner, "Settings", DECOR_TITLE|DECOR_BORDER, :height => 500)
       hFrame1 = FXHorizontalFrame.new(self)
       vFrame1 = FXVerticalFrame.new(hFrame1)
           hFrame1Label1 = FXLabel.new(vFrame1, "Google username:",:padTop => 4)
@@ -49,11 +49,17 @@ class Settings < FXDialogBox
             @GSpreadsheetKey.text = gSpreadsheetKeyConfigValue
           end
         end
-      hFrame2 = FXHorizontalFrame.new(self)
-        acceptButton = FXButton.new(hFrame2, "Accept", nil, self, ID_ACCEPT,
+      hFrame2 = FXHorizontalFrame.new(self, :opts => LAYOUT_FILL_X|LAYOUT_FIX_HEIGHT, :height => 250)
+        @maconomyData = FXDataTarget.new("")
+        maconomyBox = FXGroupBox.new(hFrame2, "Maconomy Codes", GROUPBOX_NORMAL|LAYOUT_FILL_X|LAYOUT_FILL_Y|FRAME_GROOVE)
+        maconomyFrame = FXHorizontalFrame.new(maconomyBox, FRAME_SUNKEN|FRAME_THICK|LAYOUT_FILL_X|LAYOUT_FILL_Y)
+        FXText.new(maconomyFrame, @maconomyData, FXDataTarget::ID_VALUE, TEXT_WORDWRAP|LAYOUT_FILL_X|LAYOUT_FILL_Y)
+        
+      hFrame3 = FXHorizontalFrame.new(self)
+        acceptButton = FXButton.new(hFrame3, "Accept", nil, self, ID_ACCEPT,
           FRAME_RAISED|FRAME_THICK|LAYOUT_CENTER_Y)
         acceptButton.setDefault
         acceptButton.setFocus
-        FXLabel.new(hFrame2, "Note : place the current montly worksheet as the first one")
+        FXLabel.new(hFrame3, "Note : place the current montly worksheet as the first one")
   end
 end
